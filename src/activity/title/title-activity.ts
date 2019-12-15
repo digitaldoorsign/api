05ed668f29c1d4a58b9b3cfcff2cb62579e5b5b4
Joseph Naberhaus <naberhausj@gmail.com>
2019-12-14 20:18:32 -0600

    feat(title-activity.ts): update title-activity with new generics
    The generics on crud-activity were changed to enforce the requirments better.

--- src/activity/title/title-activity.ts
+++ src/activity/title/title-activity.ts
@@ -1,6 +1,8 @@
 import {CrudActivity} from "../crud-activity";
-import {Title} from "./title";
+import {Title, TitleRecord} from "./title";
 
 export class TitleActivity extends CrudActivity<Title> {
-
+	constructor() {
+		super(TitleRecord);
+	}
 }
